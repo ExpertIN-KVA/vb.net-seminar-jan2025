cec6602b296d58b9f152ce8128b91daa6d583e25
Anastasios Tsalmas <tsalmanastasios@gmail.com>
2025-01-11 14:34:29 +0200

    clickme update

---
+++ clickme_game/clickme_menu_not-in-class-menuStrip/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Timer1 = New Timer(components)
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        ResetToolStripMenuItem = New ToolStripMenuItem()
+        EndToolStripMenuItem = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.OrangeRed
+        Button1.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(12, 57)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(280, 61)
+        Button1.TabIndex = 0
+        Button1.Text = "0"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.OrangeRed
+        Button2.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(12, 124)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(280, 61)
+        Button2.TabIndex = 1
+        Button2.Text = "5"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.DarkRed
+        Button3.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button3.ForeColor = Color.White
+        Button3.Location = New Point(12, 195)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(280, 61)
+        Button3.TabIndex = 2
+        Button3.Text = "Restart"
+        Button3.UseVisualStyleBackColor = False
+        Button3.Visible = False
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 1000
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(304, 24)
+        MenuStrip1.TabIndex = 3
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ResetToolStripMenuItem, EndToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' ResetToolStripMenuItem
+        ' 
+        ResetToolStripMenuItem.Name = "ResetToolStripMenuItem"
+        ResetToolStripMenuItem.ShortcutKeys = Keys.F5
+        ResetToolStripMenuItem.Size = New Size(180, 22)
+        ResetToolStripMenuItem.Text = "Reset"
+        ' 
+        ' EndToolStripMenuItem
+        ' 
+        EndToolStripMenuItem.Name = "EndToolStripMenuItem"
+        EndToolStripMenuItem.Size = New Size(180, 22)
+        EndToolStripMenuItem.Text = "End"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.OrangeRed
+        ClientSize = New Size(304, 268)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Click me!"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ResetToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EndToolStripMenuItem As ToolStripMenuItem
+
+End Class
---
+++ clickme_game/clickme_menu_not-in-class-menuStrip/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Randomize()     ' κάνει την Rnd() να παράγει πραγματικά τυχαίους αριθμούς
+
+        If (Timer1.Enabled = False) Then
+            Timer1.Enabled = True
+        End If
+
+        ' Button1.Text = Button1.Text + 1
+        ' Button1.Text = Rnd()
+
+
+        ' Παραγωγή τυχαίων αριθμών από το 0 έως το 99
+        ' Button1.Text = Int(Rnd() * 100)
+
+        ' Παραγωγή τυχαίων αριθμών από το 1 έως το 100
+        ' Button1.Text = Int(Rnd() * 100) + 1
+
+        ' Παραγωγή τυχαίων αριθμών από το 0 έως το 2
+        ' Button1.Text = Int(Rnd() * 3)
+
+        ' Παραγωγή τυχαίων αριθμών από το 1 έως το 3
+        ' Button1.Text = Int(Rnd() * 3) + 1
+
+        Button1.Text = Button1.Text + 1
+
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim d As Integer
+
+        a = Int(Rnd() * 100) + 1
+        b = Int(Rnd() * 100) + 1
+        c = Int(Rnd() * 100) + 1
+        d = Int(Rnd() * 100) + 1
+
+        Button1.Left = a
+        Button1.Top = b
+        Button1.Width = c
+        Button1.Height = d
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Button2.Text = Button2.Text - 1
+
+        If (Button2.Text = "0") Then
+            Timer1.Enabled = False
+            Button1.Enabled = False
+            MsgBox("To timer σταμάτησε, πέρασαν 5 δευτερόλεπτα")
+            Button3.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        Button1.Enabled = True
+        Button3.Visible = False
+
+        Button1.Text = 0
+        Button2.Text = 5
+
+
+        Button1.Width = 280
+        Button1.Height = 61
+        Button1.Left = 12
+        Button1.Top = 57
+
+        MsgBox("Το πρόγραμμα μόλις έγινε Reset και μπορείς να παίξεις ξανά")
+
+    End Sub
+
+    Private Sub ResetToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ResetToolStripMenuItem.Click
+
+        If (Timer1.Enabled = False) Then
+            Button1.Enabled = True
+            Button3.Visible = False
+
+            Button1.Text = 0
+            Button2.Text = 5
+
+            Button1.Width = 280
+            Button1.Height = 61
+            Button1.Left = 12
+            Button1.Top = 57
+
+            MsgBox("Το πρόγραμμα μόλις έγινε Reset και μπορείς να παίξεις ξανά")
+        End If
+
+    End Sub
+
+    Private Sub EndToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EndToolStripMenuItem.Click
+        End
+    End Sub
+End Class
+
